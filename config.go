43274ac1c98781a63f709386bb1d98e51a4be078
zhangyuming <zhangyuming@hanhua.com>
2018-08-21 22:50:14 +0800

    配置文件校验功能

---
+++ config.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"gopkg.in/yaml.v2"
+	"utilens/vlog"
+	"encoding/json"
+	"io/ioutil"
+	"os/user"
+	"path/filepath"
+	"os"
+	"errors"
+)
+
+const (
+	T_D = "dir"
+	T_F = "file"
+)
+
+var defaultConfDirs = extDefaultConfDir([]string{
+	"/etc/logpack",
+})
+
+type ConfModel struct {
+	typ string
+	path string
+}
+
+
+//扩展默认加载配置文件的目录支持家目录加载
+func extDefaultConfDir(defaultConfDirs []string) []string{
+	user,err := user.Current()
+	if err != nil{
+		vlog.Error("获取用户家目录失败, 将忽略默认家目录配置文件加载")
+		return defaultConfDirs
+	}
+	extConfdirs := defaultConfDirs
+	for _,d := range defaultConfDirs{
+		extConfdirs = append(extConfdirs,filepath.Join(user.HomeDir,d))
+	}
+	return extConfdirs
+}
+
+
+
+// 加载目录， 递归加载
+func loadDir(d string) ([]*Conf, error) {
+	confs := make([]*Conf,0)
+	f, err := os.Stat(d)
+	if (! os.IsNotExist(err)) && f.IsDir() {
+		files, err := ioutil.ReadDir(d)
+		if err != nil {
+			vlog.Info("dir load fail the path is : ", f.Name() , err)
+			return nil,err
+		}
+		if len(files) == 0 {
+			vlog.Debug("the path not config file:",d)
+			return nil,errors.New("the path is empty: " + d)
+		}
+		for _, f := range files {
+			if f.IsDir() {
+				p := filepath.Join(d,f.Name())
+				cs,err := loadDir(p);
+				if err == nil{
+					confs = append(confs,cs...)
+					continue
+				}else{
+					vlog.Error("dir load fail",err)
+					continue
+				}
+			}else{
+				c,err := loadConfile(filepath.Join(d,f.Name()))
+				if err != nil{
+					vlog.Error("config file load failed",err)
+					continue
+				}else{
+					confs = append(confs,c)
+					continue
+				}
+			}
+		}
+	} else {
+		vlog.Debug("path: " + d + " not exist")
+		return nil,err
+	}
+	return confs,err
+}
+
+// 加载配置文件，到 confs
+func loadConfile(fl string) (*Conf,error ){
+	conf := &Conf{}
+	bt,err := ioutil.ReadFile(fl)
+	if err != nil {
+		vlog.Error("the path " + fl + " load fail")
+		return nil,err
+	} else {
+		if err := yaml.Unmarshal(bt, &conf); err != nil {
+			vlog.Error(" path : " + fl + "解析失败 ", err)
+			return nil,err
+		}else{
+			if validateConf(conf) {
+				return conf,nil
+			}else{
+				return nil,errors.New("文件检验失败: "+fl)
+			}
+		}
+
+	}
+}
+
+//校验配置文件，archive 必填项 schedule和dirs  logrotate必填项为schedule和files
+func validateConf(conf *Conf) bool{
+
+	if conf.Archives != nil{
+		for i,a := range conf.Archives{
+			if empty(a.Schedule) || empty(a.Dirs) || len(a.Dirs) == 0{
+				bt,_ := json.Marshal(a)
+				vlog.Error("invalidate archive config , schedule and dirs by need ",string(bt))
+				conf.Archives = append(conf.Archives[:i],conf.Archives[i+1:]...)
+			}
+		}
+		if len(conf.Archives) == 0{
+			conf.Archives = nil
+		}
+	}
+	if conf.Logrotates != nil{
+		for i,v:= range conf.Logrotates {
+			if empty(v.Schedule) || empty(v.Files) || len(v.Files) == 0 {
+				bt,_ := json.Marshal(v)
+				vlog.Error("invalidate logrotate config ,schedule and files by need" , string(bt))
+				conf.Logrotates = append(conf.Logrotates[:i], conf.Logrotates[i+1:]...)
+			}
+		}
+		if (len(conf.Logrotates) == 0 ){
+			conf.Logrotates = nil
+		}
+	}
+
+	if conf.Archives == nil && conf.Logrotates == nil{
+		return false
+	}
+	return true
+}
+
+func empty(v interface{}) bool  {
+	if(v == nil){
+		return true
+	}
+	if k,ok := v.(string); ok{
+		return "" == k
+	}
+
+	return false
+}
--- main.go
+++ main.go
@@ -5,48 +5,33 @@ import (
 	"flag"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"os"
 	"utilens/vlog"
-	"path/filepath"
-	"os/user"
 	"github.com/robfig/cron"
 	"errors"
-)
-
-const (
-	T_D = "dir"
-	T_F = "file"
+	"fmt"
+	"os"
 )
 
 var lcron = cron.New()
 
 var confMode ConfModel
 
-var defaultConfDirs = extDefaultConfDir([]string{
-	"/etc/logpack",
-})
-
-type ConfModel struct {
-	typ string
-	path string
-}
-
-
-
+var confdirFlag string
+var conffileFlag string
+var validateConfFlag bool
 func init() {
-	var confdir string
-	var conffile string
-
 
 	//加载配置文件， 默认加载路径 /etc/logpack、 当前用户工作空间 ~/etc/logpack 如果通过 -f 指定那么会只加载指定的配置文件，以上配置文件目录会被忽略
-	flag.StringVar(&confdir, "d", "", "指定配置文件路径，默认会地柜加载指定路径所有文件")
-	flag.StringVar(&conffile, "f", "", "指定唯一的配置文件。 如果指定 -f 那么-d将会忽略")
+	flag.StringVar(&confdirFlag, "d", "", "指定配置文件路径，默认会地柜加载指定路径所有文件")
+	flag.StringVar(&conffileFlag, "f", "", "指定唯一的配置文件。 如果指定 -f 那么-d将会忽略")
 	flag.IntVar(&compressRate,"rate", 9, "自定压缩比率")
-	if "" == conffile { //加载文件夹
-		if "" != confdir {
+	flag.BoolVar(&validateConfFlag,"t",false,"校验配置文件")
+	flag.Parse()
+	if "" == conffileFlag { //加载文件夹
+		if "" != confdirFlag {
 			confMode = ConfModel{
 				typ : T_D,
-				path: confdir,
+				path: confdirFlag,
 			}
 			return
 		} else {
@@ -70,138 +55,14 @@ func init() {
 		}
 	} else { //加载指定的文件
 		confMode = ConfModel{
-			typ:T_F,
-			path:conffile,
+			typ:  T_F,
+			path: conffileFlag,
 		}
 		return
 	}
 
 }
 
-//扩展默认加载配置文件的目录支持家目录加载
-func extDefaultConfDir(defaultConfDirs []string) []string{
-	user,err := user.Current()
-	if err != nil{
-		vlog.Error("获取用户家目录失败, 将忽略默认家目录配置文件加载")
-		return defaultConfDirs
-	}
-	extConfdirs := defaultConfDirs
-	for _,d := range defaultConfDirs{
-		extConfdirs = append(extConfdirs,filepath.Join(user.HomeDir,d))
-	}
-	return extConfdirs
-}
-
-
-// 加载目录， 递归加载
-func loadDir(d string) ([]*Conf, error) {
-	confs := make([]*Conf,0)
-	f, err := os.Stat(d)
-	if (! os.IsNotExist(err)) && f.IsDir() {
-		files, err := ioutil.ReadDir(d)
-		if err != nil {
-			vlog.Info("dir load fail the path is : ", f.Name() , err)
-			return nil,err
-		}
-		if len(files) == 0 {
-			vlog.Debug("the path not config file:",d)
-			return nil,errors.New("the path is empty: " + d)
-		}
-		for _, f := range files {
-			if f.IsDir() {
-				p := filepath.Join(d,f.Name())
-				cs,err := loadDir(p);
-				if err == nil{
-					confs = append(confs,cs...)
-					continue
-				}else{
-					vlog.Error("dir load fail",err)
-					continue
-				}
-			}else{
-				c,err := loadConfile(filepath.Join(d,f.Name()))
-				if err != nil{
-					vlog.Error("config file load failed",err)
-					continue
-				}else{
-					confs = append(confs,c)
-					continue
-				}
-			}
-		}
-	} else {
-		vlog.Debug("path: " + d + " not exist")
-		return nil,err
-	}
-	return confs,err
-}
-
-// 加载配置文件，到 confs
-func loadConfile(fl string) (*Conf,error ){
-	conf := &Conf{}
-	bt,err := ioutil.ReadFile(fl)
-	if err != nil {
-		vlog.Error("the path " + fl + " load fail")
-		return nil,err
-	} else {
-		if err := yaml.Unmarshal(bt, &conf); err != nil {
-			vlog.Error(" path : " + fl + "解析失败 ", err)
-			return nil,err
-		}else{
-			if validateConf(conf) {
-				return conf,nil
-			}else{
-				return nil,errors.New("文件检验失败: "+fl)
-			}
-		}
-
-	}
-}
-
-//校验配置文件，archive 必填项 schedule和dirs  logrotate必填项为schedule和files
-func validateConf(conf *Conf) bool{
-
-	if conf.Archives != nil{
-		for i,a := range conf.Archives{
-			if empty(a.Schedule) || empty(a.Dirs) || len(a.Dirs) == 0{
-				bt,_ := json.Marshal(a)
-				vlog.Error("invalidate archive config , schedule and dirs by need ",string(bt))
-				conf.Archives = append(conf.Archives[:i],conf.Archives[i+1:]...)
-			}
-		}
-		if len(conf.Archives) == 0{
-			conf.Archives = nil
-		}
-	}
-	if conf.Logrotates != nil{
-		for i,v:= range conf.Logrotates {
-			if empty(v.Schedule) || empty(v.Files) || len(v.Files) == 0 {
-				bt,_ := json.Marshal(v)
-				vlog.Error("invalidate logrotate config ,schedule and files by need" , string(bt))
-				conf.Logrotates = append(conf.Logrotates[:i], conf.Logrotates[i+1:]...)
-			}
-		}
-		if (len(conf.Logrotates) == 0 ){
-			conf.Logrotates = nil
-		}
-	}
-
-	if conf.Archives == nil && conf.Logrotates == nil{
-		return false
-	}
-	return true
-}
-
-func empty(v interface{}) bool  {
-	if(v == nil){
-		return true
-	}
-	if k,ok := v.(string); ok{
-		return "" == k
-	}
-
-	return false
-}
 
 func restartCron(confs []*Conf)  {
 
@@ -246,36 +107,57 @@ func restartCron(confs []*Conf)  {
 
 }
 
-func start()  {
+func wrapperConfs() ([]*Conf,error) {
 	confs := make([]*Conf,0)
 	if confMode.typ == T_F{
 		conf,err := loadConfile(confMode.path)
 		if err != nil{
 			vlog.Error("配置文件加载失败",confMode.path,err)
-			return
+			return nil,err
 		}else{
 			confs = append(confs,conf)
+			return confs,nil
 		}
 	}else if confMode.typ == T_D{
 		cs,err := loadDir(confMode.path)
 		if err != nil{
 			vlog.Error("配置文件加载失败",confMode.path,err)
-			return
+			return nil,err
 		}else{
 			confs = append(confs,cs...)
+			return confs,nil
 		}
 	}
-	if len(confs) >0{
-		restartCron(confs)
-	}else{
-		vlog.Error("cron 启动失败")
-	}
+	return nil,errors.New("wrapper confs failed")
 }
 
 
 func main() {
 
-	start()
+	if validateConfFlag{
+		confs,err := wrapperConfs()
+		if err == nil{
+			for _,c := range confs{
+				bt,err := yaml.Marshal(c)
+				if err != nil{
+					vlog.Error("序列化 文件失败",err)
+				}
+				if !validateConf(c){
+					fmt.Println("validate failed\n",string(bt))
+					os.Exit(1)
+				}
+			}
+		}else{
+			fmt.Println("validate failed",err)
+			os.Exit(2)
+		}
+		fmt.Println("sccessful")
+		os.Exit(0)
+	}
+	confs,err := wrapperConfs()
+	if err == nil{
+		restartCron(confs)
+	}
 
 	select {
 
